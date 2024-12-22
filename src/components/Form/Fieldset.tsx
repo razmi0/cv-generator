1064e73bec872b9153b6d97ff018dfe10db57004
razmiO <thomas.cuesta31@gmail.com>
2024-12-22 12:37:44 +0100

    Refactor Fieldset component to accept dynamic input types

--- src/components/Form/Fieldset.tsx
+++ src/components/Form/Fieldset.tsx
@@ -1,22 +1,35 @@
 /** @jsx h */
 
+import formfields from "@/components/Form/fields.ts";
 import Input from "@/components/Form/Input.tsx";
 import { h } from "@land/jsx";
-
 type InputGroup = {
     label: string;
     id: string;
     name: string;
 }[];
 
-type FieldsetProps = {
+type FieldsetProps<T extends keyof CvType> = {
     _legend: string;
-    inputs: InputGroup;
+    inputs: T extends "header"
+        ? typeof formfields.header
+        : T extends "competences"
+        ? typeof formfields.competences
+        : T extends "experiences"
+        ? typeof formfields.experiences
+        : T extends "formations"
+        ? typeof formfields.formations
+        : never;
     children?: JSX.Element;
-    id?: keyof CvType;
+    id: T;
 };
 
-export default function Fieldset({ _legend, inputs, children, id }: FieldsetProps) {
+export default function Fieldset<T extends keyof CvType>({
+    _legend,
+    inputs,
+    children,
+    id,
+}: FieldsetProps<T>): JSX.Element {
     return (
         <fieldset id={id}>
             <legend>{_legend}</legend>
