7a75b45a4c3a1a558e168e6516c05a4ac4142c61
razmiO <thomas.cuesta31@gmail.com>
2024-12-23 13:30:17 +0100

    refactor(main.ts) : extract logic in one funtion run

--- main.ts
+++ main.ts
@@ -12,78 +12,77 @@ Deno.log = log;
 const askFileWithExtension = (pathname: string) => !!pathname.match(/\.\w+$/);
 const links = ["/index.html", "/exercice.html", "/build-cv/form"];
 
-Deno.serve(async (req) => {
-    //
-    //
-    const run = async (): Promise<Response> => {
-        const pathname = new URL(req.url).pathname;
-        const route = `${req.method.toUpperCase()}:${pathname}`;
+async function run(req: Request): Promise<Response> {
+    const pathname = new URL(req.url).pathname;
+    const route = `${req.method.toUpperCase()}:${pathname}`;
 
-        Deno.record(["route", route], ["pathname", pathname]);
+    Deno.record(["route", route], ["pathname", pathname]);
 
-        switch (route) {
-            // Pages
-            // - Home
-            case "GET:/":
-                return await Route.serveStatic("src/pages", "index.html", req);
+    switch (route) {
+        // Pages
+        // - Home
+        case "GET:/":
+            return await Route.serveStatic("src/pages", "index.html", req);
 
-            // - Home
-            case "GET:/index.html":
-                return await Route.serveStatic("src/pages", "index.html", req);
+        // - Home
+        case "GET:/index.html":
+            return await Route.serveStatic("src/pages", "index.html", req);
 
-            // - Exercice
-            case "GET:/exercice.html":
-                return await Route.serveStatic("src/pages", "exercice.html", req);
+        // - Exercice
+        case "GET:/exercice.html":
+            return await Route.serveStatic("src/pages", "exercice.html", req);
 
-            // SSR ROUTES
-            case "GET:/build-cv/form": {
-                const response = await Route.ssrFormPage({
-                    method: "POST",
-                    action: "/build-cv/submit",
-                    links,
+        // SSR ROUTES
+        case "GET:/build-cv/form": {
+            const response = await Route.ssrFormPage({
+                method: "POST",
+                action: "/build-cv/submit",
+                links,
+            });
+            return response;
+        }
+
+        case "POST:/build-cv/submit": {
+            try {
+                const data = (await req.json()) as Partial<CvType>;
+                const cvData = CvSchema.parse(data);
+                const cvPage = cvTemplate.page(cvData);
+                return new Response(cvPage, {
+                    headers: {
+                        "Content-Type": "text/html",
+                    },
                 });
-                return response;
-            }
-
-            case "POST:/build-cv/submit": {
-                try {
-                    const data = (await req.json()) as Partial<CvType>;
-                    const cvData = CvSchema.parse(data);
-                    const cvPage = cvTemplate.page(cvData);
-                    return new Response(cvPage, {
-                        headers: {
-                            "Content-Type": "text/html",
-                        },
-                    });
-                } catch (e) {
-                    if (e instanceof z.ZodError) {
-                        // Return validation error details
-                        console.log(e.errors);
-                        return new Response(JSON.stringify(e.errors), { status: 400 });
-                    }
-                    throw e; // Re-throw if it's not a Zod error
+            } catch (e) {
+                if (e instanceof z.ZodError) {
+                    // Return validation error details
+                    return new Response(JSON.stringify(e.errors), { status: 400 });
                 }
-            }
-            // END SSR ROUTES
-
-            // Assets
-            // - css, js
-            default: {
-                if (askFileWithExtension(pathname)) {
-                    const response = await Route.serveStatic("public/assets", pathname, req);
-                    Deno.record(["assets", "true"]);
-                    return response;
-                }
-                Deno.record(["assets", "false"]);
-                return Route.notFound();
+                throw e; // Re-throw if it's not a Zod error
             }
         }
-    };
+        // END SSR ROUTES
 
-    //
-    //
+        // Assets
+        // - css, js
+        default: {
+            if (askFileWithExtension(pathname)) {
+                const response = await Route.serveStatic("public/assets", pathname, req);
+                Deno.record(["assets", "true"]);
+                return response;
+            }
+            Deno.record(["assets", "false"]);
+            return Route.notFound();
+        }
+    }
+}
 
-    const response = await run();
+Deno.serve({ port: 5050 }, async (req) => {
+    const pathname = new URL(req.url).pathname;
+    const route = `${req.method.toUpperCase()}:${pathname}`;
+    Deno.record(["route", route], ["pathname", pathname]);
+
+    const response = await run(req);
+
     Deno.record(["response", response.status]);
     Deno.log();
     return response;
