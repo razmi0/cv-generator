374edd1c0d077b2e62756af329e62ca877c8f4a1
razmiO <thomas.cuesta31@gmail.com>
2024-12-27 13:38:17 +0100

    Refactor Nav.tsx to remove slashes from link labels

--- src/components/ui/Nav.tsx
+++ src/components/ui/Nav.tsx
@@ -4,12 +4,16 @@
 import { Fragment, h } from "@land/jsx";
 
 export default function Nav({ links }: { links: string[] }) {
+    const labels = links.map((l) => {
+        return l.replace(/\//g, "");
+    });
+
     return (
         <>
             <nav>
-                {links.map((l) => (
+                {links.map((l, i) => (
                     <li>
-                        <a href={l}>@{l}__</a>
+                        <a href={l}>@{labels[i]}__</a>
                     </li>
                 ))}
             </nav>
